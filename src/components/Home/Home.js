8461c73e5bc2f212718c3899b77d70c541956694
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-06-01 23:42:48 -0500

    create mock navigation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
+import { Label } from 'office-ui-fabric-react/lib/Label'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Label disabled={true}>I'm a disabled Label</Label>
     </div>
   )
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,8 @@
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar'
+import React from 'react'
+
+const Navigation = () => {
+  return <CommandBar />
+}
+
+export default Navigation
