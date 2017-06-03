53aadd1e5c52e767a5f2e0bb666ba7aa04fd65c6
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-06-02 19:52:49 -0500

    test client entry point

--- src/client.js
+++ src/client.js
@@ -1,13 +1,14 @@
 import BrowserRouter from 'react-router-dom/BrowserRouter'
 import Home from './components/Home/Home'
 import Layout from './components/Layout/Layout'
+import Navigation from './components/Navigation/'
 import React from 'react'
 import { render } from 'react-dom'
 
 render(
   <BrowserRouter>
     <Layout>
-      <Home />
+      <h3>client test</h3>
     </Layout>
   </BrowserRouter>,
   document.getElementById('root')
